927dce7762947d8a94538bb0332d1448a61ade8e
fallenleavesbuy <9261562+fallenleavesguy@users.noreply.github.com>
2025-05-23 22:13:18 +0800

    data persistence

--- AvaloniaSimpleToDoList/App.axaml.cs
+++ AvaloniaSimpleToDoList/App.axaml.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
@@ -10,12 +12,13 @@ namespace AvaloniaSimpleToDoList;
 
 public partial class App : Application
 {
+    private readonly MainWindowViewModel _mainWindowViewModel = new MainWindowViewModel();
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
     }
 
-    public override void OnFrameworkInitializationCompleted()
+    public override async void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
@@ -24,10 +27,44 @@ public partial class App : Application
             BindingPlugins.DataValidators.RemoveAt(0);
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(),
+                DataContext = _mainWindowViewModel,
             };
+            desktop.ShutdownRequested += DesktopOnShutdownRequested;
         }
 
         base.OnFrameworkInitializationCompleted();
+        await InitMainViewModelAsync();
+    }
+
+    private bool _canClose = false;
+
+    private async void DesktopOnShutdownRequested(object? sender, ShutdownRequestedEventArgs e)
+    {
+        e.Cancel = !_canClose;
+
+        if (!_canClose)
+        {
+            var itemsToSave = _mainWindowViewModel.ToDoItems.Select(item => item.GetToDoItem());
+            await ToDoListFileService.SaveToFileAsync(itemsToSave);
+
+            _canClose = true;
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.Shutdown();
+            }
+        }
+    } 
+
+    private async Task InitMainViewModelAsync()
+    {
+        var itemsLoaded = await ToDoListFileService.LoadFromFileAsync();
+
+        if (itemsLoaded != null)
+        {
+            foreach (var item in itemsLoaded)
+            {
+                _mainWindowViewModel.ToDoItems.Add(new ToDoItemViewModel(item));
+            }
+        }
     }
 }
