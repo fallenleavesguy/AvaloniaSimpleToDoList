93d72bfa77122cf75f34eeef026dc080f4e770aa
fallenleavesbuy <9261562+fallenleavesguy@users.noreply.github.com>
2025-05-26 11:19:42 +0800

    update

---
+++ FuncDataTemplateSample/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using FuncDataTemplateSample.Models;
+
+namespace FuncDataTemplateSample.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Sex2 { get; } = "";
+    public List<Person> People { get; } = new List<Person>()
+    {
+        new Person
+        {
+            FirstName = "Mr.",
+            LastName = "X",
+            Sex=Sex.Diverse
+        },
+        new Person
+        {
+            FirstName = "Hello",
+            LastName = "World",
+            Sex= Sex.Male
+        },
+        new Person
+        {
+            FirstName = "Hello",
+            LastName = "Kitty",
+            Sex= Sex.Female
+        }
+    };
+}
